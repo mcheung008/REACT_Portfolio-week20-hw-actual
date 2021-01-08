11c86f210719f5d5be798e1c1c77e2aca18736be
mcheung008 <mcheung008@gmail.com>
2021-01-08 13:03:55 -0800

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Footer from './components/Footer';
 import './App.css';
 
 class App extends React.Component {
@@ -7,8 +11,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Michael Cheung'
-      headerLinks [
+      title: 'Michael Cheung',
+      headerLinks: [
         { title: "Portfolio", path: "/" },
         { title: "About Me", path: "/about" },
         { title: "Contact", path: "/contact" }
@@ -29,6 +33,24 @@ class App extends React.Component {
   render() {
     return ( 
       <Router>
+        <Container className="p-0" fluid={true}>
+          
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Michael Cheung</Navbar.Brand>
+            
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Portfolio</Link>
+                <Link className="nav-link" to="/about">About Me</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+            <Footer />
+
+          </Container>
 
          </Router>
     );
